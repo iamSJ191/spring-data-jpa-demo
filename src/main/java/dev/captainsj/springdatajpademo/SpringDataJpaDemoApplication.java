b425511c458173848e27d7c6effdb3719c90230c
CaptainSJ <i.saurabhjoshi@hotmail.com>
2023-02-04 12:03:07 +0530

    added faker to load sample data

--- src/main/java/dev/captainsj/springdatajpademo/SpringDataJpaDemoApplication.java
+++ src/main/java/dev/captainsj/springdatajpademo/SpringDataJpaDemoApplication.java
@@ -1,11 +1,41 @@
 package dev.captainsj.springdatajpademo;
 
+import com.github.javafaker.Faker;
+import dev.captainsj.springdatajpademo.model.Student;
+import dev.captainsj.springdatajpademo.repository.StudentRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringDataJpaDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringDataJpaDemoApplication.class, args);
+    }
+
+        @Bean
+        CommandLineRunner runner(StudentRepository studentRepository) {
+            return args -> {
+                Faker faker = new Faker();
+
+                for (int i = 0; i < 20; i++) {
+                    Student student = new Student(
+                    faker.name().firstName(),
+                    faker.name().lastName(),
+                    faker.name().firstName() + faker.name().lastName()+"@sj.com",
+                    faker.random().nextInt(5,25)
+                    );
+
+                    studentRepository.save(student);
+                }
+
+
+                studentRepository.findAll().forEach(System.out::println);
+
+            };
+
+
+
     }
 }
